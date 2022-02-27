1b4b7f81db118a998be6ed0f125b04690462be09
moritzmhmk <moritzmhmk@gmail.com>
2022-02-27 17:58:07 +0000

    adjustments to make build work

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #rm -rf build
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
-(cd build && emconfigure cmake ..) &&
+(cd build && emcmake cmake ..) &&
 (cd build && emmake make VERBOSE=1 -j 16)
 cp ./build/src/libjpegturbowasm.js ./dist && 
 cp ./build/src/libjpegturbowasm.wasm ./dist && 
