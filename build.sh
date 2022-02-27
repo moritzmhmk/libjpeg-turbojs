cad8b6d339efc439bf98174a80fc86904f3fa342
moritzmhmk <moritzmhmk@gmail.com>
2022-02-27 18:15:21 +0000

    build only wasm

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-#rm -rf build
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
+
 (cd build && emcmake cmake ..) &&
 (cd build && emmake make VERBOSE=1 -j 16)
-cp ./build/src/libjpegturbowasm.js ./dist && 
-cp ./build/src/libjpegturbowasm.wasm ./dist && 
-cp ./build/src/libjpegturbojs.js.mem ./dist &&
 cp ./build/src/libjpegturbojs.js ./dist &&
+cp ./build/src/libjpegturbojs.wasm ./dist &&
+
 (cd test/node; npm run test)
