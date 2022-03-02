c7d2a8a4c4a8c45f4f40b71dc5c4c0ba5f6fdc84
moritzmhmk <moritzmhmk@gmail.com>
2022-03-02 08:27:27 +0000

    add web build

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 mkdir -p build
 #(cd build && emconfigure cmake -DCMAKE_BUILD_TYPE=Debug ..) &&
 
-(cd build && emcmake cmake ..) &&
+(cd build && emcmake cmake -DBUILD_WEB=OFF ..) &&
 (cd build && emmake make VERBOSE=1 -j 16)
 cp ./build/src/libjpegturbojs.js ./dist &&
 cp ./build/src/libjpegturbojs.wasm ./dist &&
+
+(cd build && emcmake cmake -DBUILD_WEB=ON ..) &&
+(cd build && emmake make VERBOSE=1 -j 16)
+cp ./build/src/libjpegturbojs.js ./dist/libjpegturbojs.web.js &&
 
 (cd test/node; npm run test)
